88b5c1a804a583b88c6f65cfbf02e5f4c0935f29
Saumit Dinesan <justsaumit@protonmail.com>
2023-05-05 19:49:17 +0530

    CamTest: fixing typos

--- CamTest.py
+++ CamTest.py
@@ -1,12 +1,14 @@
 import cv2
+
 # Setting the video capture source to default (0)
 cap = cv2.VideoCapture(0)
+
 # Setting the resolution of the video capture
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # While loop to continuously capture frames from camera
-while(True):
+while True:
     # Read a frame from the camera
     ret, frame = cap.read()
     #frame = cv2.flip(frame, -1) # Flip camera vertically
@@ -18,11 +20,11 @@ while(True):
     cv2.imshow('Grayscale', gray)
     
     # Wait for 30 milliseconds for a key event (extract sigfigs) and exit if 'ESC' or 'q' is pressed
-    k = cv2.waitKey(30) & 0xff
-    if key == 27: # ESCAPE key
+    key = cv2.waitKey(30) & 0xff
+    if key == 27:  # ESCAPE key
         break
-    elif key == 113 # q key
-    break
+    elif key == 113:  # q key
+        break
 
 # Release the camera and close all windows
 cap.release()
